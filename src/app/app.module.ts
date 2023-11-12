bb90cc3d2f04cae3500495885672c476d90aa8d7
cang8 <0.gizemgunes@gmail.com>
2023-11-12 23:22:55 +0300

    httpClient-get

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { UiModule } from './ui/ui.module';
 import { ToastrModule } from 'ngx-toastr';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { BaseComponent } from './base/base.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -20,9 +21,12 @@ import { BaseComponent } from './base/base.component';
     AppRoutingModule,
     AdminModule, UiModule,
     ToastrModule.forRoot(),
-    NgxSpinnerModule
+    NgxSpinnerModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    { provide: "baseUrl", useValue: "https://localhost:7136/api", multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
