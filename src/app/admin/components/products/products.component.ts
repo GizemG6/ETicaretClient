ea49bb0570344afb3da11334da31ca4291827c43
cang8 <0.gizemgunes@gmail.com>
2023-11-14 23:06:59 +0300

    add-post-put-delete-contracts

--- src/app/admin/components/products/products.component.ts
+++ src/app/admin/components/products/products.component.ts
@@ -2,6 +2,7 @@ import { NgxSpinnerService } from 'ngx-spinner';
 import { Component, OnInit } from '@angular/core';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 import { HttpClientService } from 'src/app/services/common/http-client.service';
+import { Product } from 'src/app/contracts/product';
 
 @Component({
   selector: 'app-products',
@@ -16,9 +17,35 @@ export class ProductsComponent extends BaseComponent implements OnInit {
 
   ngOnInit(): void {
     this.showSpinner(SpinnerType.BallAtom);
-    this.httpClientService.get({
+
+    this.httpClientService.get<Product[]>({
       controller: "products"
     }).subscribe(data => console.log(data));
+
+    /* this.httpClientService.post({
+      controller: "products"
+    }, {
+      name: "Kalem",
+      stock: 100,
+      price: 15
+    }).subscribe(); */
+
+    /* this.httpClientService.put({
+      controller: "products"
+    }, {
+      id: "6869efa1-eba6-4c29-9717-42849ffe7ea1",
+      name: "Renkli Kağıt",
+      stock: 1500,
+      price: 5.5
+    }).subscribe(); */
+
+    /* this.httpClientService.delete({
+      controller: "products"
+    }, "id...").subscribe(); */
+
+    /* this.httpClientService.get({
+      fullEndPoint: "https://jsonplaceholder.typicode.com/posts"
+    }).subscribe(data => console.log(data)); */
   }
 
 }
