3e7a23523284c15f8687c41e92c5f23cfb60475b
cang8 <0.gizemgunes@gmail.com>
2023-11-18 23:37:46 +0300

    create-product

---
+++ src/app/admin/components/products/create/create.component.ts
@@ -0,0 +1,39 @@
+import { NgxSpinnerService } from 'ngx-spinner';
+import { Component, OnInit } from '@angular/core';
+import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
+import { Create_Product } from 'src/app/contracts/create_product';
+import { AlertifyService, MesssageType, Position } from 'src/app/services/admin/alertify.service';
+import { ProductService } from 'src/app/services/common/models/product.service';
+
+@Component({
+  selector: 'app-create',
+  templateUrl: './create.component.html',
+  styleUrls: ['./create.component.css']
+})
+export class CreateComponent extends BaseComponent implements OnInit{
+  constructor(spiner: NgxSpinnerService, private productService: ProductService, private alerfity: AlertifyService) {
+    super(spiner)
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+  create(name: HTMLInputElement, stock: HTMLInputElement, price: HTMLInputElement) {
+    this.showSpinner(SpinnerType.BallAtom);
+    const create_product: Create_Product = new Create_Product();
+    create_product.name = name.value;
+    create_product.stock = parseInt(stock.value);
+    create_product.price = parseFloat(price.value);
+
+    this.productService.create(create_product, () => {
+      this.hideSpinner(SpinnerType.BallAtom);
+      this.alerfity.message("Urun basariyla eklendi", {
+        dismissOthers: true,
+        messageType: MesssageType.Success,
+        position: Position.TopRight,
+        delay: 0
+      });
+    });
+  }
+}
--- src/app/admin/components/products/products.component.ts
+++ src/app/admin/components/products/products.component.ts
@@ -2,7 +2,7 @@ import { NgxSpinnerService } from 'ngx-spinner';
 import { Component, OnInit } from '@angular/core';
 import { BaseComponent, SpinnerType } from 'src/app/base/base.component';
 import { HttpClientService } from 'src/app/services/common/http-client.service';
-import { Product } from 'src/app/contracts/product';
+import { Create_Product } from 'src/app/contracts/create_product';
 
 @Component({
   selector: 'app-products',
@@ -18,7 +18,7 @@ export class ProductsComponent extends BaseComponent implements OnInit {
   ngOnInit(): void {
     this.showSpinner(SpinnerType.BallAtom);
 
-    this.httpClientService.get<Product[]>({
+    this.httpClientService.get<Create_Product[]>({
       controller: "products"
     }).subscribe(data => console.log(data));
 
