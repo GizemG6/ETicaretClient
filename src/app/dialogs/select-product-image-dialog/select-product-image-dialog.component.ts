1545249cf3ee8a165ec0f8825d219656afa1ea3d
cang8 <0.gizemgunes@gmail.com>
2024-01-14 21:36:59 +0300

    add-deleteimage-on-productservice

--- src/app/dialogs/select-product-image-dialog/select-product-image-dialog.component.ts
+++ src/app/dialogs/select-product-image-dialog/select-product-image-dialog.component.ts
@@ -4,6 +4,8 @@ import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dial
 import { FileUploadOptions } from 'src/app/services/common/file-upload/file-upload.component';
 import { ProductService } from 'src/app/services/common/models/product.service';
 import { List_Product_Image } from 'src/app/contracts/list_product_image';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { SpinnerType } from 'src/app/base/base.component';
 
 @Component({
   selector: 'app-select-product-image-dialog',
@@ -13,7 +15,8 @@ import { List_Product_Image } from 'src/app/contracts/list_product_image';
 export class SelectProductImageDialogComponent extends BaseDialog<SelectProductImageDialogComponent> implements OnInit{
   constructor(dialogRef: MatDialogRef<SelectProductImageDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: SelectProductImageState | string,
-    private productService: ProductService)
+    private productService: ProductService,
+    private spinner: NgxSpinnerService)
     {
       super(dialogRef)
     }
@@ -30,7 +33,12 @@ export class SelectProductImageDialogComponent extends BaseDialog<SelectProductI
     images: List_Product_Image[];
 
     async ngOnInit() {
-      this.images = await this.productService.readImages(this.data as string);
+      this.images = await this.productService.readImages(this.data as string, () => this.spinner.hide(SpinnerType.BallAtom));
+    }
+
+    async deleteImage(imageId: string) {
+      this.spinner.show(SpinnerType.BallAtom);
+      await this.productService.deleteImage(this.data as string, imageId, () => this.spinner.hide(SpinnerType.BallAtom));
     }
 }
 
