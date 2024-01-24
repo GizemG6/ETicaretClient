af148645db62965e2e69537e60d83adf35fe047c
cang8 <0.gizemgunes@gmail.com>
2024-01-24 18:00:12 +0300

    added-formbuilder-to-registercomponentts

--- src/app/ui/components/register/register.component.ts
+++ src/app/ui/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit{
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
+  frm: FormGroup;
   ngOnInit(): void {
-    
+    this.frm = this.formBuilder.group({
+      adSoyad: [""],
+      kullaniciAdi: [""],
+      email: [""],
+      sifre: [""],
+      sifreTekrar: [""]
+    })
   }
 }
