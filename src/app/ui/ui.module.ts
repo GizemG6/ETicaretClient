609282c53d51346299ce1807b219b298a89ecc87
cang8 <0.gizemgunes@gmail.com>
2024-02-03 18:55:48 +0300

    added-ui-component-login-module

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ComponentsModule } from './components/components.module';
+import { LoginComponent } from './component/login/login.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent
+  ],
   imports: [
     CommonModule,
     ComponentsModule
